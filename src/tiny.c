8e0752033b1f4e7809a518df8f7d1a5cbe946802
Bryce Mecum <petridish@gmail.com>
2026-01-27 21:37:14 -0800

    Edits to comments

--- src/tiny.c
+++ src/tiny.c
@@ -5,18 +5,19 @@
 // doesn't specifically say which you have to define or in what order you should
 // tackle them as you build a driver.
 //
-// While working with [validation](https://github.com/adbc-drivers/validation)
-// framework for the [ADBC Driver Foundry](https://adbc-drivers.org), I got to
-// wondering about this question.
+// While working with the
+// [validation](https://github.com/adbc-drivers/validation) framework for the
+// [ADBC Driver Foundry](https://adbc-drivers.org), I got to wondering about
+// this question.
 //
 // ADBC drivers can be implemented a few ways, and one of the most common is to
 // build them as a shared library that exports symbols from the ADBC C API. This
-// shared library is then be loaded by an ADBC Driver Manager and the program
-// calls into the driver through the Driver Manager.
+// shared library is then loaded by an ADBC Driver Manager and the program calls
+// into the driver through the Driver Manager.
 //
 // I remembered seeing a comment somewhere that a Driver Manager, when loading a
-// driver, can actually fill in some methods for a driver that doesn't
-// implement. This gave me an idea for how to approach my original question.
+// driver, can actually fill in some methods for a driver that doesn't implement
+// them. This gave me an idea for how to approach my original question.
 //
 // ## The Plan
 //
@@ -26,7 +27,7 @@
 // [adbc-driver-manager](https://pypi.org/project/adbc-driver-manager/) which
 // wraps the C++ driver manager.
 //
-// To build and test the driver, run,
+// When all is done, I'll build and test the driver by running:
 //
 // ```sh
 // $ make
@@ -38,21 +39,25 @@
 
 // ## A Tiny Driver
 //
-// Given our rules above, let's walk through what it takes to build a very tiny
-// ADBC driver. For simplicity, it's not going to connect to any external or
-// in-memory database but it will load successfully with the driver manager.
+// I'll walk through what it takes to build a very tiny ADBC driver. For
+// simplicity, the driver isn't going to connect to any external or in-memory
+// database or do much of anything but it will load successfully with the driver
+// manager.
 
-// TODO: Talk about me
+// I'll include this because I need to allocate at least some memory to uphold
+// the API contract.
 #include <stdlib.h>
 
-// The first line of code we're write is to include the ADBC header:
+// Then I'll include the ADBC C header. I've vendored it in this repo for
+// simplicity.
 #include "../vendor/adbc/adbc.h"
 
-// I mentioned below a set of functions and here they are. This is the
-// minimum set of ADBC functions a driver has to implement to be loadable.
-// Here I forward declare them so we can see them all together at once and
-// we'll actually define their bodies later on. We use ADBC_EXPORT which is
-// defined in the ADBC header to ensure proper linkage across platforms.
+// Earlier I mentioned some 52 functions in the ADBC API. After doing some
+// testing, I narrowed down the minimum set to just 13 functions.
+//
+// Here I forward declare them so you can see them all together at once and I'll
+// actually define their bodies later on. To ensure the driver manager can find
+// these symbols, each is marked with `ADBC_EXPORT`.
 ADBC_EXPORT AdbcStatusCode AdbcDriverInit(int version, void *raw_driver,
                                           struct AdbcError *error);
 ADBC_EXPORT AdbcStatusCode AdbcDatabaseNew(struct AdbcDatabase *database,
@@ -89,21 +94,20 @@ ADBC_EXPORT AdbcStatusCode AdbcStatementExecuteQuery(
 
 // ## Implementing The Driver
 //
-// At this point, we're ready to start filling in our ADBC function bodies. I'll
-// explain a bit about each as we go and also about what the relevant parts of
-// the API contract are.
+// Now on to implementation. I'll explain a bit about each as I go and also
+// about what the relevant parts of the API contract are.
 
-// `AdbcDatabaseNew` typically allocates an `AdbcDatabase` after which the
-// Database would be in a created but unitialized state. Our driver can just
+// `AdbcDatabaseNew` normally allocates an `AdbcDatabase` after which the
+// Database would be in a created but uninitialized state. My driver can just
 // return `ADBC_STATUS_OK`.
 ADBC_EXPORT AdbcStatusCode AdbcDatabaseNew(struct AdbcDatabase *database,
                                            struct AdbcError *error) {
   return ADBC_STATUS_OK;
 }
 
-// We have to define `AdbcDatabaseSetOption` but we should return
-// `ADBC_STATUS_NOT_IMPLEMENTED` to make it clear that calling this won't do
-// anything.
+// The driver manager requires drivers to define `AdbcDatabaseSetOption` but
+// it's most correct to return `ADBC_STATUS_NOT_IMPLEMENTED` to make it clear
+// that calling this won't do anything.
 ADBC_EXPORT AdbcStatusCode AdbcDatabaseSetOption(struct AdbcDatabase *database,
                                                  const char *key,
                                                  const char *value,
@@ -111,24 +115,25 @@ ADBC_EXPORT AdbcStatusCode AdbcDatabaseSetOption(struct AdbcDatabase *database,
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// `AdbcDatabaseInit` typically does any final prep before initializing the
-// database with the options we may have set. Since our driver doesn't support
-// options, we can just return `ADBC_STATUS_OK`.
+// `AdbcDatabaseInit` normally does any final prep before initializing the
+// database with the options that may have been set. Since my driver doesn't
+// support options, I just return `ADBC_STATUS_OK`.
 ADBC_EXPORT AdbcStatusCode AdbcDatabaseInit(struct AdbcDatabase *database,
                                             struct AdbcError *error) {
   return ADBC_STATUS_OK;
 }
 
-// `AdbcDatabaseRelease` would typically ensure any open connections are cleaned
-// up before freeing its own resources but our driver can just mark this as
-// `ADBC_STATUS_NOT_IMPLEMENTED`.
+// `AdbcDatabaseRelease` would ensure any open connections are cleaned up
+// before freeing its own resources but my driver can just mark this as
+// `ADBC_STATUS_NOT_IMPLEMENTED` since there are no resources to free in this
+// driver.
 ADBC_EXPORT AdbcStatusCode AdbcDatabaseRelease(struct AdbcDatabase *database,
                                                struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
 // The driver manager treats any driver that doesn't set `private_data` to a
-// non-null pointer as unitialized (unusable) so we just do a tiny allocation
+// non-null pointer as uninitialized (unusable) so I just do a tiny allocation
 // here to make it happy.
 ADBC_EXPORT AdbcStatusCode AdbcConnectionNew(struct AdbcConnection *connection,
                                              struct AdbcError *error) {
@@ -140,15 +145,16 @@ ADBC_EXPORT AdbcStatusCode AdbcConnectionNew(struct AdbcConnection *connection,
   return ADBC_STATUS_OK;
 }
 
-// Similar to `AdbcDatabaseInit`, we can just return `ADBC_STATUS_OK`.
+// Similar to `AdbcDatabaseInit`, I just return `ADBC_STATUS_OK` for
+// `AdbcConnectionInit`.
 ADBC_EXPORT AdbcStatusCode AdbcConnectionInit(struct AdbcConnection *connection,
                                               struct AdbcDatabase *database,
                                               struct AdbcError *error) {
   return ADBC_STATUS_OK;
 }
 
-// As part of the API contract, `AdbcConnectionRelease` needs to release the
-// `private_data` member as part of the API contract.
+// Because `AdbcConnectionNew` allocated `private_data` and, as part of the API
+// contract, `AdbcConnectionRelease` needs to release `private_data`.
 ADBC_EXPORT AdbcStatusCode AdbcConnectionRelease(
     struct AdbcConnection *connection, struct AdbcError *error) {
   if (connection->private_data) {
@@ -159,54 +165,53 @@ ADBC_EXPORT AdbcStatusCode AdbcConnectionRelease(
   return ADBC_STATUS_OK;
 }
 
-// We can leave this as `ADBC_STATUS_NOT_IMPLEMENTED` just like we did
-// `AdbcDatabaseSetOption`.
+// I can leave this as `ADBC_STATUS_NOT_IMPLEMENTED` just like
+// `AdbcDatabaseSetOption` since it won't do anything.
 ADBC_EXPORT AdbcStatusCode
 AdbcConnectionSetOption(struct AdbcConnection *connection, const char *key,
                         const char *value, struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// The next three functions can just return `ADBC_STATUS_OK`. Typically, these
-// would allocate, modify, and release `private_data` related to our statement.
+// The next three functions can just return `ADBC_STATUS_OK`. Normally, these
+// would allocate, modify, and release `private_data` related to a statement
+// that doesn't apply since this driver doesn't execute any queries.
 ADBC_EXPORT AdbcStatusCode AdbcStatementNew(struct AdbcConnection *connection,
                                             struct AdbcStatement *statement,
                                             struct AdbcError *error) {
   return ADBC_STATUS_OK;
 }
 
+// Because `AdbcStatementNew` didn't do anything, `AdbcStatementRelease` doesn't
+// need to do anything either.
 ADBC_EXPORT AdbcStatusCode AdbcStatementRelease(struct AdbcStatement *statement,
                                                 struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// Normally this would take a query but our driver already knows the answer to
-// any query we could conceivably send to it so we can just return
-// `ADBC_STATUS_OK`.
+// Same for `AdbcStatementSetSqlQuery`.
 ADBC_EXPORT AdbcStatusCode
 AdbcStatementSetSqlQuery(struct AdbcStatement *statement, const char *query,
                          struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// Finally, a function that does something! Here we just create our simple
-// `ArrowArrayStream`.
+// and `AdbcStatementExecuteQuery`.
 ADBC_EXPORT AdbcStatusCode AdbcStatementExecuteQuery(
     struct AdbcStatement *statement, struct ArrowArrayStream *out,
     int64_t *rows_affected, struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// This isn't part of the ADBC API but the driver manager will want us to define
-// this.
+// This isn't part of the ADBC API but the driver manager will need this to be
+// defined.
 AdbcStatusCode AdbcDriverRelease(struct AdbcDriver *driver,
                                  struct AdbcError *error) {
   return ADBC_STATUS_NOT_IMPLEMENTED;
 }
 
-// This is a special function the driver manager will call after it loads our
-// driver which helps the driver manager find all of the functions we just
-// defined.
+// Last is `AdbcDriverInit`. When the driver manager loads a driver, it calls
+// this function to set up its mappings to the functions in it.
 ADBC_EXPORT
 AdbcStatusCode AdbcDriverInit(int version, void *raw_driver,
                               struct AdbcError *error) {
@@ -232,23 +237,27 @@ AdbcStatusCode AdbcDriverInit(int version, void *raw_driver,
   return ADBC_STATUS_OK;
 }
 
-// And we're done. We've defined just enough of an ADBC driver for it to
-// cross-platform, loadable by a driver manager.
+// And that's it.
 //
-// To build the driver for your platform, run:
+// The driver manager should now be able to load this driver once it's built.
+//
+// To build the driver, run:
 //
 // ```sh
 // $ make build
 // ```
 //
-// and then to test the driver, you can open up a Python REPL with
-// `adbc_driver_manager` with,
+// I tried to make this work in a cross-platform way so hopefully this will work
+// for you.
+//
+// To test the driver, open up a Python REPL with the `adbc_driver_manager`
+// available:
 //
 // ```sh
 // uv run --with "adbc_driver_manager" python
 // ```
 //
-// and then in your REPL, execute:
+// and then execute:
 //
 // ```python
 // from adbc_driver_manager import dbapi
@@ -258,8 +267,4 @@ AdbcStatusCode AdbcDriverInit(int version, void *raw_driver,
 //         pass
 // ```
 //
-// Or just run the automated tests with `uv run pytest`.
-
-// ## Summary
-//
-// TODO
+// Alternatively, run the automated tests with `uv run pytest`.
